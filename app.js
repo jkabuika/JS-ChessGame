d6d4a35b5b990c0f2606070d5a2da685c90b036a
jkabuika <jkabuika1319@gmail.com>
2024-05-15 16:13:08 -0400

    Added start pieces to app.js

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ const infoDisplay = document.querySelector("#info-display")
 const width = 8
 
 const startPieces = [
-  
+  rook, knight, bishop, queen, king, bishop, knight, rook,
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+  rook, knight, bishop, queen, king, bishop, knight, rook,
 ]
