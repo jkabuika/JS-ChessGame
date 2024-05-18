fe8f5b74505c2ac746d01aa1671967595d6ae9f3
jkabuika <jkabuika1319@gmail.com>
2024-05-18 15:43:00 -0400

    Styled the drag images and added functionality to remember pieces being dragged (which box they come from)

--- app.js
+++ app.js
@@ -42,3 +42,31 @@ function createBoard() {
 }
 
 createBoard()
+
+
+
+
+const allSquares = document.querySelectorAll("#gameboard .square")
+
+allSquares.forEach(square => {
+  square.addEventListener('dragstart', dragStart)
+  square.addEventListener('dragover', dragOver)
+  square.addEventListener('drop', dragDrop)
+
+})
+
+let startPositionId
+let draggedElement
+
+function dragStart (e) {
+  startPositionId = e.target.parentNode.getAttribute('square.id')
+  draggedElement = e.target
+}
+
+function dragOver(e) {
+  e.preventDefault()
+}
+
+function dragDrop() {
+  
+}
